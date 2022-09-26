a1838991033a65cce75051950f1fc4dc174d6861
Mahdi Eltayeb <114348932+mahdiSnowflakeTraining@users.noreply.github.com>
2022-09-26 08:07:50 -0500

    Update streamlit_app.py
    added api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd
 
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -16,3 +17,7 @@ fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#new section displaying fruityvice api response
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
